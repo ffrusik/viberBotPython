a6b0d8a3d1f2676858b175f90600c06f5f555f90
ffrusik <superruslan2005@gmail.com>
2021-08-15 19:35:56 +0300

    final

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
-from flask import Flask, request, Response
+from flask import Flask, request, Response, logging
+from viberbot.api.messages import TextMessage
+from viberbot.api.viber_requests import ViberMessageRequest, ViberSubscribedRequest, ViberFailedRequest
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 bot_configuration = BotConfiguration(
 	name='Bot',
@@ -13,11 +22,34 @@ app = Flask(__name__)
 
 @app.route('/incoming', methods=['POST'])
 def incoming():
-	logger.debug("received request. post data: {0}".format(request.get_data()))
-	# handle the request here
+	if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+		return Response(status=403)
+
+	# this library supplies a simple way to receive a request object
+
+	viber_request = viber.parse_request(request.get_data())
+
+	if isinstance(viber_request, ViberMessageRequest):
+		message = viber_request.message
+		viber.send_messages(viber_request.sender.id, [
+			message
+		])
+
+	elif isinstance(viber_request, ViberSubscribedRequest):
+		viber.send_messages(viber_request.user.id, [
+			TextMessage(text="thanks for subscribing!")
+		])
+
+	elif isinstance(viber_request, ViberFailedRequest):
+		logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+
 	return Response(status=200)
 
-context = ('server.crt', 'server.key')
-app.run(host='0.0.0.0', port=443, debug=True, ssl_context=context)
+	# logger.debug("received request. post data: {0}".format(request.get_data()))
+	# # handle the request here
+	# return Response(status=200)
 
-viber.set_webhook('https://:443/')
+context = ('server.crt', 'server.key')
+app.run(host='0.0.0.0', port=8443, debug=True, ssl_context=context)
+
+viber.set_webhook('https://aqueous-tundra-56533.herokuapp.com')
